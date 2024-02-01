1b2cb1201ed5581adcb1ccebb28dfc0e9c2f2a8d
Nagaev Dmitry <dinvk@mail.ru>
2024-02-01 05:52:47 +0500

    Exclude inactive hosts while host booking creation (#23)

--- haxball_site/reservation/admin.py
+++ haxball_site/reservation/admin.py
@@ -4,13 +4,19 @@ from .models import ReservationHost, ReservationEntry, Replay
 
 # Register your models here.
 
-admin.site.register(ReservationHost)
-# admin.site.register(ReservationEntry)
 admin.site.register(Replay)
 
+@admin.register(ReservationHost)
+class ReservationHostAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_active')
 
 @admin.register(ReservationEntry)
-class PlayerAdmin(admin.ModelAdmin):
-    list_display = ("author", "match", "time_date", "host", "created")
+class ReservationEntryAdmin(admin.ModelAdmin):
+    list_display = ('author', 'match', 'time_date', 'host', 'created')
     raw_id_fields = ('match',)
     list_filter = ('host', 'author')
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "host":
+            kwargs["queryset"] = ReservationHost.objects.filter(is_active=True)
+        return super(ReservationEntryAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
