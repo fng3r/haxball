3496b9de4a863f448bf9f61be2494a6f161236a9
Alex-Ish <alexishin64@gmail.com>
2024-02-02 02:13:56 +0400

    update users profile achievements and add category medals, add team achievements

--- haxball_site/tournament/models.py
+++ haxball_site/tournament/models.py
@@ -429,7 +429,7 @@ class Achievements(models.Model):
         return self.title
 
     class Meta:
-        ordering = ['position_number']
+        ordering = ['category__order','position_number']
         verbose_name = 'Медалька'
         verbose_name_plural = 'Медальки'
 
