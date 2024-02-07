9a51b7ed151f3768639e1c4fa6eba2640d5f6b9e
Alex-Ish <alexishin64@gmail.com>
2024-02-07 18:33:25 +0400

    add/fix review

--- haxball_site/static/scripts/ProfileScripts.js
+++ haxball_site/static/scripts/ProfileScripts.js
@@ -1,17 +1,15 @@
 
-    console.log("страница загружена");
-    let buttonProfileInfo = document.querySelector('.profile-tabs');
-    let buttonProfileAchievements = document.querySelectorAll('.profile-tabs');
-    let secondElement = buttonProfileAchievements[1];
-    console.log(secondElement);
+    let ProfileTabs = document.querySelectorAll('.profile-tabs');
+    let firstElement = ProfileTabs[0];
+    let secondElement = ProfileTabs[1];
 
-    buttonProfileInfo.addEventListener("click", function(event){
-        document.getElementById('profile-info-tab').style.color = 'black';
+    firstElement.addEventListener("click", function(event){
+        document.getElementById('profile-info-tab').style.color = '#000000';
         document.getElementById('profile-achievements-tab').style.color = '#007bff';
     });
 
 
     secondElement.addEventListener("click",function(event){
-        document.getElementById('profile-achievements-tab').style.color = 'black';
+        document.getElementById('profile-achievements-tab').style.color = '#000000';
         document.getElementById('profile-info-tab').style.color = '#007bff';
     });
