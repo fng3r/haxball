f5f18d7b777e5f4b0974753f68d1436aa0eb5086
Alex-Ish <alexishin64@gmail.com>
2024-03-08 20:26:34 +0400

    add visual for nickname history

--- haxball_site/static/scripts/ProfileScripts.js
+++ haxball_site/static/scripts/ProfileScripts.js
@@ -2,14 +2,38 @@
     let ProfileTabs = document.querySelectorAll('.profile-tabs');
     let firstElement = ProfileTabs[0];
     let secondElement = ProfileTabs[1];
+    let historyNicknamesButton = document.querySelector('.nickname-inline');
+    let nicknamesFlag = 1;
 
     firstElement.addEventListener("click", function(event){
         document.getElementById('profile-info-tab').style.color = '#000000';
         document.getElementById('profile-achievements-tab').style.color = '#007bff';
     });
 
-
     secondElement.addEventListener("click",function(event){
         document.getElementById('profile-achievements-tab').style.color = '#000000';
         document.getElementById('profile-info-tab').style.color = '#007bff';
     });
+
+    /*historyNicknamesButton.addEventListener("mouseover", function(event){
+        document.querySelector('.history-nicknames').style.opacity = '0.8';
+    });
+
+    historyNicknamesButton.addEventListener("mouseout", function(event){
+        document.querySelector('.history-nicknames').style.opacity = '0';
+    });*/
+
+    historyNicknamesButton.addEventListener("click",function(event){
+        if(nicknamesFlag){
+            document.querySelector('.history-nicknames').style.opacity = '0.8';
+            document.querySelector('.profile-treangle').style.transform = 'rotate(180deg)';
+            nicknamesFlag = 0;
+        } else {
+            document.querySelector('.history-nicknames').style.opacity = '0';
+            document.querySelector('.profile-treangle').style.transform = 'rotate(0deg)';
+            nicknamesFlag = 1;
+        }
+
+
+    });
+
