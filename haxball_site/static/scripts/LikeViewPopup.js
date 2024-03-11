69518b7f338ea0a5aba0e89f3a9a391a8c733e08
fng3r <dinvk@mail.ru>
2024-03-11 06:52:17 +0500

    make it work

--- haxball_site/static/scripts/LikeViewPopup.js
+++ haxball_site/static/scripts/LikeViewPopup.js
@@ -1,20 +1,19 @@
+document.querySelectorAll('.popup-tab').forEach(popupTab => {
+    let popupTabLike = popupTab.querySelector('.popup-tab-like');
+    let popupTabDisLike = popupTab.querySelector('.popup-tab-dislike');
 
-    let tabPopupLike = document.querySelector('.popup-tab-like');
-    let tabPopupDisLike = document.querySelector('.popup-tab-dislike');
-
-    tabPopupLike.addEventListener("click", function(event){
-        document.querySelector('.popup-tab-like').style.boxShadow = '0px 0px 5px #00FF00';
-        document.querySelector('.popup-tab-dislike').style.boxShadow = '0px 0px 3px #fbd784';
+    popupTabLike.addEventListener("click", function(event) {
+        popupTabLike.style.boxShadow = '0px 0px 5px #00FF00';
+        popupTabDisLike.style.boxShadow = '0px 0px 3px #fbd784';
         $('.vote-dislike').fadeOut(0);
         $('.vote-like').fadeIn(0);
     });
 
-
-    tabPopupDisLike.addEventListener("click", function(event){
-        document.querySelector('.popup-tab-dislike').style.boxShadow = '0px 0px 5px #bd2130';
-        document.querySelector('.popup-tab-like').style.boxShadow = '0px 0px 3px #fbd784';
+    popupTabDisLike.addEventListener("click", function(event){
+        popupTabDisLike.style.boxShadow = '0px 0px 5px #bd2130';
+        popupTabLike.style.boxShadow = '0px 0px 3px #fbd784';
         $('.vote-like').fadeOut(0);
         $('.vote-dislike').fadeIn(0);
-
     });
+});
 
