0f188e47d016721e7c3edd074b4c186b3855a399
Alex-Ish <alexishin64@gmail.com>
2024-03-15 09:41:22 +0400

    update script tabs

--- haxball_site/static/scripts/LikeViewPopup.js
+++ haxball_site/static/scripts/LikeViewPopup.js
@@ -2,18 +2,43 @@ document.querySelectorAll('.popup-tab').forEach(popupTab => {
     let popupTabLike = popupTab.querySelector('.popup-tab-like');
     let popupTabDisLike = popupTab.querySelector('.popup-tab-dislike');
 
+
     popupTabLike.addEventListener("click", function(event) {
-        popupTabLike.style.boxShadow = '0px 0px 5px #00FF00';
+        let x = 0;
+        if(event.target.parentElement.classList == "popup-tab-like"){
+            x = event.target.parentElement.id;
+        } else {
+            x = event.target.id;
+        }
+
+        document.getElementById(x).style.boxShadow = '0px 0px 5px #00FF00';
+        document.getElementById('dis' + x).style.boxShadow = '0px 0px 3px #fbd784';
+        document.getElementById('vote-' + x).style.display = 'block';
+        document.getElementById('vote-dis' + x).style.display = 'none';
+
+        /*popupTabLike.style.boxShadow = '0px 0px 5px #00FF00';
         popupTabDisLike.style.boxShadow = '0px 0px 3px #fbd784';
         $('.vote-dislike').fadeOut(0);
-        $('.vote-like').fadeIn(0);
+        $('.vote-like').fadeIn(0);*/
+
     });
 
     popupTabDisLike.addEventListener("click", function(event){
-        popupTabDisLike.style.boxShadow = '0px 0px 5px #bd2130';
-        popupTabLike.style.boxShadow = '0px 0px 3px #fbd784';
-        $('.vote-like').fadeOut(0);
-        $('.vote-dislike').fadeIn(0);
+        let x = 0;
+
+        if(event.target.parentElement.classList == "popup-tab-dislike"){
+            x = event.target.parentElement.id;
+        } else {
+            x = event.target.id;
+        }
+
+        x = x.replace('dis', '');
+
+        document.getElementById(x).style.boxShadow = '0px 0px 3px #fbd784';
+        document.getElementById('dis' + x).style.boxShadow = '0px 0px 5px #bd2130';
+        document.getElementById('vote-' + x).style.display = 'none';
+        document.getElementById('vote-dis' + x).style.display = 'block';
+
     });
 });
 
