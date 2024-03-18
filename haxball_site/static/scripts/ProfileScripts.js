857c7d618f4e47b352bb01ec8395969f60321148
fng3r <dinvk@mail.ru>
2024-03-19 04:01:38 +0500

    display previous user nicknames

--- haxball_site/static/scripts/ProfileScripts.js
+++ haxball_site/static/scripts/ProfileScripts.js
@@ -1,15 +1,33 @@
 
     let ProfileTabs = document.querySelectorAll('.profile-tabs');
-    let firstElement = ProfileTabs[0];
-    let secondElement = ProfileTabs[1];
+    let firstTab = ProfileTabs[0];
+    let secondTab = ProfileTabs[1];
 
-    firstElement.addEventListener("click", function(event){
+    firstTab.addEventListener("click", function(event){
         document.getElementById('profile-info-tab').style.color = '#000000';
         document.getElementById('profile-achievements-tab').style.color = '#007bff';
     });
 
 
-    secondElement.addEventListener("click",function(event){
+    secondTab.addEventListener("click",function(event){
         document.getElementById('profile-achievements-tab').style.color = '#000000';
         document.getElementById('profile-info-tab').style.color = '#007bff';
+    });
+
+
+    let historyNicknamesButton = document.querySelector('.nickname-inline');
+    let nicknamesFlag = 1;
+
+    historyNicknamesButton.addEventListener("click",function(event){
+        if(nicknamesFlag){
+            document.querySelector('.history-nicknames').style.opacity = '0.8';
+            document.querySelector('.profile-triangle').style.transform = 'rotate(180deg)';
+            nicknamesFlag = 0;
+        } else {
+            document.querySelector('.history-nicknames').style.opacity = '0';
+            document.querySelector('.profile-triangle').style.transform = 'rotate(0deg)';
+            nicknamesFlag = 1;
+        }
+
+
     });
